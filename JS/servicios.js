c695b7bc8c0a9f19253f102d17b1606e24321c84
cabonillamo <106216123+cabonillamo@users.noreply.github.com>
2022-08-10 18:25:49 -0600

    Add files via upload

---
+++ JS/servicios.js
@@ -0,0 +1,10 @@
+const scannerButton = document.querySelector("scanner");
+const diagnosticoButton = document.querySelector("diganostico");
+const mantenimientoButton = document.querySelector("mantenimiento");
+const otrosServiButton= document.querySelector("otros-servi");
+
+function cambiar() {
+    if(scannerButton == onclick) {
+        document.getElementById
+    }
+}
